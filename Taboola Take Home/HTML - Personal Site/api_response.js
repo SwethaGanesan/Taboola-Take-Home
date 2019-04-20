da321767b040cdf9ce0be6fc64a1d050b535557a
Swetha Ganesan <swethaganesan89@gmail.com>
2019-04-20 13:03:44 -0700

    After review comments

--- Taboola Take Home/HTML - Personal Site/api_response.js
+++ Taboola Take Home/HTML - Personal Site/api_response.js
@@ -1,48 +1,73 @@
+function checkCountry(position) {
+
+  var top = 51.3457868; //north lat
+  var left = -127.7844079; //west long
+  var right = -69.9513812; //east long
+  var bottom =  24.7433195; //south
+
+  if(left<=position.coords.longitude && position.coords.longitude<=right && bottom<=position.coords.latitude && position.coords.latitude===top){
+    console.log("US");
+  }
+  else{
+    console.log("not in the US");
+    var header = document.querySelector(".leftheader");
+    //Display Hindi if not in the US
+    header.textContent = decodeURIComponent(escape("\xE0\xA4\xB5\xE0\xA5\x87\xE0\xA4\xAC\x20\xE0\xA4\x95\xE0\xA5\x87\x20\xE0\xA4\x86\xE0\xA4\xB8\xE0\xA4\xAA\xE0\xA4\xBE\xE0\xA4\xB8\x20\xE0\xA4\xB8"));
+  }
+}
 var xhr = new XMLHttpRequest();
 xhr.onload = function() {
-  if(xhr.status == 200){
+  if(xhr.status === 200){
     console.log('success');
-    console.log(this.responseText);
-    var myObj = JSON.parse(this.responseText);
+    console.log(xhr.responseText);
+    var myObj = JSON.parse(xhr.responseText);
     console.log(myObj);
 
-    if(myObj != null){
+    if(myObj !== null){
       var list = myObj.list;
-      console.log(list);
-      if (list.length != 6){
-        console.log("Could not process request to get 6 ads");
+      if (list.length < 6){
+        console.log("Could not process request to get at least 6 ads");
       }
       else{
-        var branding=[];
-        var name=[];
-        var thumbnailimage=[];
-        var url=[];
+
+        var brandingElements = document.querySelectorAll(".branding");
+        var titleElements = document.querySelectorAll(".title");
+        var thumbNailElements = document.querySelectorAll(".thumbnail");
 
         try{
         for(var i=0; i<6; i++){
 
-          branding.push(list[i].branding);
-          console.log(branding[i]);
-          document.querySelectorAll(".branding")[i].textContent = branding[i];
+          brandingElements[i].textContent = list[i].branding;
+          titleElements[i].textContent = list[i].name;
+          thumbNailElements[i].src = list[i].thumbnail[0].url;
 
-          name.push(list[i].name);
-          console.log(name[i]);
-          document.querySelectorAll(".title")[i].textContent = name[i];
+          if (list[i].categories !== undefined && list[i].categories.length !== 0){
+            brandingElements[i].textContent = list[i].categories[0].charAt(0).toUpperCase() + list[i].categories[0].substr(1) + " | " +  brandingElements[i].textContent;
+          }
 
-          thumbnailimage.push(list[i].thumbnail[0].url);
-          console.log(thumbnailimage[i]);
-          document.querySelectorAll(".thumbnail")[i].src = thumbnailimage[i];
+          var linkTitle = document.createElement("a");
+          linkTitle.href = list[i].url;
+          linkTitle.target = "_blank";
+          linkTitle.innerHTML=titleElements[i].innerHTML;
+          titleElements[i].replaceChild(linkTitle,titleElements[i].childNodes[0]);
 
-          url.push(list[i].url);
-          console.log(url[i]);
+          var linkBrand = document.createElement("a");
+          linkBrand.href = list[i].url;
+          linkBrand.target = "_blank";
+          linkBrand.innerHTML = brandingElements[i].innerHTML;
+          brandingElements[i].replaceChild(linkBrand,brandingElements[i].childNodes[0]);
+
+          var linkImg = document.createElement("a");
+          linkImg.href = list[i].url;
+          linkImg.target = "_blank";
+          var parent = thumbNailElements[i].parentNode;
+          parent.replaceChild(linkImg, thumbNailElements[i]);
+          linkImg.appendChild(thumbNailElements[i]);
 
         }}
-        catch(Exception){
-          console.log("Couldn't process API request");
+        catch(e){
+          console.log(e.stack);
         }
-
-
-
     }
     }
 
@@ -52,64 +77,16 @@ xhr.onload = function() {
   }
 };
 
+
+
+
+if ("geolocation" in navigator) {
+  //geolocation is available
+  navigator.geolocation.getCurrentPosition(checkCountry);
+} else {
+  //geolocation IS NOT available
+  console.log("Geolocation not supported");
+}
+
 xhr.open('GET','https://api.taboola.com/1.2/json/apitestaccount/recommendations.get?app.type=web&app.apikey=7be65fc78e52c11727793f68b06d782cff9ede3c&source.id=%2Fdigiday-publishing-summit%2F&source.url=https%3A%2F%2Fblog.taboola.com%2Fdigiday-publishing-summit%2F&source.type=text&placement.organic-type=mix&placement.visible=true&placement.available=true&placement.rec-count=6&placement.name=Below%20Article%20Thumbnails&placement.thumbnail.width=640&placement.thumbnail.height=480&user.session=init');
 xhr.send();
-
-//https://images.taboola.com/taboola/image/fetch/f_jpg%2Cq_auto%2Ch_480%2Cw_640%2Cc_fill%2Cg_faces:auto%2Ce_sharpen/http%3A%2F%2Fcdn.taboola.com%2Flibtrc%2Fstatic%2Fthumbnails%2Fcc0b0ea15f280ec6e40633694c793683.jpg
-//https://images.taboola.com/taboola/image/fetch/f_jpg%2Cq_auto%2Ch_480%2Cw_640%2Cc_fill%2Cg_faces:auto%2Ce_sharpen/http%3A%2F%2Ffroppitinc.com%2Fcreatives%2Fbl%2Fancmyst%2F55.jpg
-//https://images.taboola.com/taboola/image/fetch/f_jpg%2Cq_auto%2Ch_480%2Cw_640%2Cc_fill%2Cg_faces:auto%2Ce_sharpen/https%3A%2F%2Fstorage.googleapis.com%2Fcaw-uploads%2Fd6c6403e5bb75c6a39dc75c7c047b799.jpg
-//https://images.taboola.com/taboola/image/fetch/f_jpg%2Cq_auto%2Ch_480%2Cw_640%2Cc_fill%2Cg_faces:auto%2Ce_sharpen/https%3A%2F%2Fcdn.science101.com%2Fwp-content%2Fuploads%2F2019%2F01%2FSquare-waves.jpg
-//https://images.taboola.com/taboola/image/fetch/f_jpg%2Cq_auto%2Ch_480%2Cw_640%2Cc_fill%2Cg_faces:auto%2Ce_sharpen/http%3A%2F%2Fcdn.taboola.com%2Flibtrc%2Fstatic%2Fthumbnails%2F8f2b1606a416f13865766591baa323ac.jpg
-//https://images.taboola.com/taboola/image/fetch/f_jpg%2Cq_auto%2Ch_480%2Cw_640%2Cc_fill%2Cg_faces:auto%2Ce_sharpen/https%3A%2F%2Fgallery-pl.go-game.io%2Fuploads%2F2019%2F01%2FVk_Resizes%25201000x600_B63497_1000x600_English%26IMG%3D1CZT.png
-
-
-
-
-
-
-
-
-
-
-//document.querySelector("div").style.backgroundColor="yellow
-//document.querySelector("button").classList.add("invisible");
-//document.querySelector("button").classList.remove("invisible");, toggle
-//document.querySelector("a").getAttribute("href")
-//document.querySelector("a").setAttribute("href", "https://bing.com")
-
-//var image1 = document.querySelectorAll("img")[0];
-//image1.setAttribute("src, randomImage");
-
-//document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
-//if(x>y)
-//{}
-//getElementById
-/*
-
-document.querySelector("button").addEventListener("click", handleClick);
-function handleClick(){
-  alert("I got clicked")
-}
-
-document.querySelector("button").addEventListener("click", function(){
-alert("I got clicked")
-});
-
-for (var i=0; i< document.querySelectorAll(".drum").length; i++){
-  document.querySelectorAll(".drum")[i].addEventListener("click", function()){
-  alert("I got clicked")
-  console.log(this.innerhtml)
-  this.style.color = "white"
-
-}
-}
-
-var housekeeper1 = new HouseKeeper(name, years)
-function HouseKeeper(name,years){
-this.name = name
-this.years = years
-
-}
-
-
-*/
